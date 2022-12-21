47e99b489984bc61fba77bfa98406a990fa67c4d
afiqadha18 <afiqadhabaharudin@gmail.com>
2022-12-21 13:58:42 +0800

    add bootstrap4

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -49,4 +49,13 @@ return array (
       '@yii/symfonymailer' => $vendorDir . '/yiisoft/yii2-symfonymailer/src',
     ),
   ),
+  'yiisoft/yii2-bootstrap4' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap4',
+    'version' => '2.0.10.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap4' => $vendorDir . '/yiisoft/yii2-bootstrap4/src',
+    ),
+  ),
 );
