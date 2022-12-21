075444f67257208d5fd23bc3ba5ad2728196a477
afiqadha18 <afiqadhabaharudin@gmail.com>
2022-12-21 15:22:05 +0800

    udpate app name

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,6 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
+    'name' => 'IPDC mHub',
     'modules' => [],
     'components' => [
         'request' => [
